05902a003276aea31a4943c7d88c4b4d9a51c9a2
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-11-12 13:02:38 -0500

    fixed a few typos

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,23 +1,26 @@
 //=== DEPENDENCIES =================================
-const Polls = require("../models/pollmodel.js");
+const Poll = require("../models/pollmodel.js");
 
 //=== ROUTES =======================================
 module.exports = function(app){
 
    //=== GET ALL THE POLLS ============================
    app.get("/api/all", (req, res)=> {
-      //SOMETHING HAPPENS
+      Poll.findAll({}).then((results)=> {
+         res.json(results);
+      });
    });
 
    //=== ADD A POLL ===================================
    app.post("/api/new", (req, res)=> {
-      Polls.create({
+      Poll.create({
          user: req.body.user,
          optionOne: req.body.optionOne,
          optionTwo: req.body.optionTwo,
          optionThree: req.body.optionThree,
          optionFour: req.body.optionFour
       }).then((results)=> {
+         //result is the newly created poll
          res.end();
       });
    });
