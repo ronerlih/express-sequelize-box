dc6303f254ab0687698dcc7b8b550cefc432dc40
ron erlih <ronerlih@gmail.com>
2020-11-17 16:21:10 -0500

    cleanup tests

--- utils/tests/seed.test.js
+++ utils/tests/seed.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable quotes */
 require("mysql2/node_modules/iconv-lite").encodingExists("cesu8");
+// let db = require("../../models");
 const seed = require("../seed.js");
-let db = require("../../models");
 
 // mock the log function to test side-effects
 global.console.log = jest.fn();
