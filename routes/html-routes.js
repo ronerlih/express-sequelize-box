07d9f33bf561c0db0f38c2e00ff4a76e702718ce
CaroS <carolyn.mary@hotmail.com>
2020-11-18 15:07:34 -0500

    polls displaying

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const app = express();
-const db = require("../models");
+const Poll = require("../models/pollmodel");
+
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function (app) {
    // GET route for getting all of the posts
    app.get("/", (req, res) => {
       res.render("index");
    });
 
    app.get("/polls", (req, res) => {
-      res.render("polls");
+      Poll.findAll({}).then((results) => {
+         res.render("polls", { polls: results });
+      });
    });
 
    // router.get("/main", (req, res) => {
